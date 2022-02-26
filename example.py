1ae4ead6aa81732c8dc265cb2ff9b62819e86bba
jcaubin <jcaubin@gmail.com>
2022-02-26 17:56:49 +0100

    En la rama master
     Confirmación inicial
    
     Cambios a ser confirmados:
    	nuevo archivo:  .gitignore
    	nuevo archivo:  Adafruit_ADS1x15.py
    	nuevo archivo:  Adafruit_I2C.py
    	nuevo archivo:  ads1x15_ex_comparator.py
    	nuevo archivo:  ads1x15_ex_differential.py
    	nuevo archivo:  ads1x15_ex_singleended.py
    	nuevo archivo:  example.py

---
+++ example.py
@@ -0,0 +1,26 @@
+#pruebas del adafruit
+
+from time import sleep
+import Adafruit_ADS1x15 as adf
+
+ADS1015 = 0x00  
+ADS1115 = 0x01 
+
+ads = adf.ADS1x15(address=0x48, ic=ADS1115)
+
+def readData():
+    d= ads.readADCSingleEnded()
+    print(f"v: {d/1000}", end='\r')
+
+
+def loop():
+    while True:
+        readData()
+        sleep(0.2)
+
+if __name__ == '__main__':     # Program start from here
+	try:
+		loop()
+	except KeyboardInterrupt:  # 
+		print('fin')
+
